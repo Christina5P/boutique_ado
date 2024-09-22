24f0cf80c968c4ee8b2c72504b7a10ccb04b7bd0
Christina Åhman <christina.ahman@tv4.se>
2024-09-22 08:11:08 +0000

    finished product admin function

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower 
@@ -71,9 +71,20 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """Add product to the store"""
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Succesfully added product')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product')
+    else:
+
+        form = ProductForm()
     template = 'products/add_product.html'
     context = {
         'form': form,
     }
-    return render (request, template, context)
+
+    return render(request, template, context)

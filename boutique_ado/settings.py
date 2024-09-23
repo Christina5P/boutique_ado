9d76c2ce7bcc003e5697c186b25037a179a38085
Christina Åhman <christina.ahman@tv4.se>
2024-09-23 18:52:53 +0000

    push

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -161,12 +161,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+      'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }   
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'pp5boutiqueadonew'
     AWS_S3_REGION_NAME = 'eu-north-1'
